61bcd56572e62759f8a3a2ed66bb12981a557390
Justin Collier <justin.collier@dcmail.ca>
2024-05-16 23:00:06 -0400

    Transcription Page Front-End
    + Front-End pages for: Transcription, Translation, Information

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,10 +1,33 @@
 import React, { useState, useEffect, useRef } from 'react'
+import Transcription from './Transcription'
+import Translation from './Translation'
 
 export default function Information(props) {
    
+    // Constants
+    const { output, finished } = props
+    const [tab, setTab] = useState('transcription')
+    const [translation, setTranslation] = useState(null)
+    const [toLanguage, setToLanguage] = useState('Select language')
+    const [translating, setTranslating] = useState(null)
+    console.log(output)
+
+    const worker = useRef()
 
     return (
-        <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 max-w-prose w-full mx-auto'>
+        <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20  mx-auto'>
+
+            <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-red-300 bold'>Transcription</span></h1>
+           
+            <div className='grid grid-cols-2 sm:mx-auto bg-white  rounded overflow-hidden items-center p-1 redShadow border-[2px] border-solid border-red-300'>
+                <button onClick={() => setTab('transcription')} className={'px-4 rounded duration-200 py-1 ' + (tab === 'transcription' ? ' bg-red-300 text-white' : ' text-red-900 hover:text-red-100')}>Transcription</button>
+                <button onClick={() => setTab('translation')} className={'px-4 rounded duration-200 py-1  ' + (tab === 'translation' ? ' bg-red-300 text-white' : ' text-red-900 hover:text-red-100')}>Translation</button>
+            </div>
+
+            {tab === 'transcription' ? 
+                (<Transcription/>) : 
+                (<Translation/>)
+            }
             
         </main>
     )
