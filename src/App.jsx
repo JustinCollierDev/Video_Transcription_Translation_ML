6ed7dee7ab724e50c612949868522dbfa3a3d09c
Justin Collier <justin.collier@dcmail.ca>
2024-05-16 21:49:40 -0400

    Audio Recording Functionality
    + Added basic level of audio input gathering/recording.
    + Added additional documentation
    + Changed the colour schema of the web app.

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,20 @@ import { useState } from 'react'
 import HomePage from './components/HomePage'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import FileDisplay from './components/FileDisplay'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  /* Constants for provided file OR recording*/
+  const [file, setAudioFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+  const isAudioAvailable = file || audioStream
+
+  function handleAudioReset() {
+    setAudioFile(null)
+    setAudioStream()
+  }null
 
   return (
     <div className='flex flex-col max-w-[1920px] mx-auto w-full'>
@@ -18,7 +29,8 @@ function App() {
         
         {/* Our Header and Main tags are held within these two .jsx files that we function call*/}
         <Header />
-        <HomePage />
+        
+        {isAudioAvailable ? (<FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={setAudioStream}/>) : (<HomePage setAudioFile={setAudioFile} setAudioStream={setAudioStream}/>)}
 
       </section>
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,32 +1,121 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
-export default function HomePage() {
+// Home page for QuickScript
+export default function HomePage(props) {
+
+  const {setAudioStream, setAudioFile} = props
+
+  // Variables for handling a recording state
+  const [recordingStatus, setRecordingState] = useState('inactive')
+  const [audioChunks, setAudioChunks] = useState([])
+  const [duraction, setDuration] = useState(0)
+
+  const mediaRecorder = useRef(null)
+  const mimeType = 'audio/webm'
+
+  // Function: Handles when the user clicks the 'recording' button to create their own audio input.
+  async function startRecording() {
+      let tempStream
+      console.log('Start recording')
+
+      // Getting access to the user's microphone or input device.
+      try {
+          const streamData = await navigator.mediaDevices.getUserMedia({
+              audio: true,
+              video: false
+          })
+          tempStream = streamData
+
+      // Catch errors if thrown.
+      } catch (err) {
+          console.log(err.message)
+          return
+      }
+
+      // Change the current recording status to active.
+      setRecordingStatus('recording')
+
+      // Recording media chunks from the active input device
+      const media = new MediaRecorder(tempStream, { type: mimeType })
+      mediaRecorder.current = media
+
+      // Begin recording the audio
+      mediaRecorder.current.start()
+      let localAudioChunks = []
+
+      mediaRecorder.current.ondataavailable = (event) => {
+          if (typeof event.data === 'undefined') { return } // Audio data cannot be read
+          if (event.data.size === 0) { return }             // Audio data is empty (size=0)
+
+          // Recording in-progress
+          localAudioChunks.push(event.data)
+      }
+      // Begin recording audio
+      setAudioChunks(localAudioChunks)
+  }
+
+  // Function: Handles when the user has finished recording and wishes to save their audio for transcription.
+  async function stopRecording() {
+
+      // Set status to inactive
+      setRecordingStatus('inactive')
+      console.log('Stop recording')
+
+      // Stop the acvtive recorder.
+      mediaRecorder.current.stop()
+      mediaRecorder.current.onstop = () => {
+
+          // Create an audioBlob instance with our recorded audioChunks
+          const audioBlob = new Blob(audioChunks, { type: mimeType })
+          setAudioStream(audioBlob) // Set the stream to our Blob
+          setAudioChunks([])
+          setDuration(0)
+      }
+  }
+
+  // Function: Counts the duration of an active recording
+  useEffect(() => {
+      if (recordingStatus === 'inactive') { return }
+
+      // Set the interval at which we count.
+      const interval = setInterval(() => {
+          setDuration(curr => curr + 1)
+      }, 1000) // 1000ms == 1 second
+
+      return () => clearInterval(interval)
+  })
+
+  // Main Form
   return (
     <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20'>
         
-        <h1 className='font-semibold text-5xl sm:text-6l md:text-7xl'>Quick<span className='text-blue-400 bold'>Script</span></h1>
+        <h1 className='font-semibold text-5xl sm:text-6l md:text-7xl'>Quick<span className='text-red-300 bold'>Script</span></h1>
     
-        <h3 className='font-medium md:text-lg'>Upload <span className='text-blue-400'> <i class="fa-solid fa-upload"></i></span>
+        <h3 className='font-medium md:text-lg'>Upload <span className='text-red-300'> <i className="fa-solid fa-upload"></i></span>
             <span className='font-medium md:text-lg'> &rarr;</span>
-            {" "}Transcribe <span className='text-blue-400'> <i class="fa-solid fa-pen-nib"> </i> </span> <span className='font-medium md:text-lg'> &rarr;</span>
-            {" "}Translate <span className='text-blue-400'> <i class="fa-solid fa-earth-americas"></i></span>
+            {" "}Transcribe <span className='text-red-300'> <i className="fa-solid fa-pen-nib"> </i> </span> <span className='font-medium md:text-lg'> &rarr;</span>
+            {" "}Translate <span className='text-red-300'> <i className="fa-solid fa-earth-americas"></i></span>
         </h3>
 
-        <label className='text-blue cursor-pointer hover:text-blue-400 duration-200'>
-            Upload <i class="fa-solid fa-upload"></i>
-            <input className='hidden' type='file' accept='.mp3, .wave, .mp4'/>
+        <label className='text-blue cursor-pointer hover:text-red-300 duration-200'>
+            Upload File <i className="fa-solid fa-upload"></i>
+
+            <input onChange={(e) =>{
+              const tempFile = e.target.files[0]
+                setAudioFile(tempFile)
+            }} className='hidden' type='file' accept='.mp3,.wave,.mp4'/>
         </label>
         
         <p className='text-base my-4'> Or </p>
 
         <button className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full'>
-          <p className='font-sm text-blue-400 text-center'>Record your voice</p>
+          <p className='font-sm text-red-300 text-center'>Record your voice</p>
           <div className='flex items-center gap-2'>
-            <i class="fa-solid fa-microphone"></i>
+            <i className="fa-solid fa-microphone"></i>
           </div>
         </button>
 
-        <p className='italic text-slate-500'>Currently supporting <span className='text-blue-400'>[ .mp4, .mp3, .wav ]</span></p>
+        <p className='italic text-slate-500'>Currently supporting <span className='text-red-300'>[ .mp4, .mp3, .wav ]</span></p>
 
         <br/><br/><br/><br/><br/>
         
